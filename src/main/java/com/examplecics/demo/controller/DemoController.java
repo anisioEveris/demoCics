367367dd818cc64654f482b362f00340a91b2def
anisioEveris <anisio.junior-ext@viavarejo.com.br>
2021-12-14 15:24:25 -0300

    Alteração Thiago

--- src/main/java/com/examplecics/demo/controller/DemoController.java
+++ src/main/java/com/examplecics/demo/controller/DemoController.java
@@ -3,10 +3,7 @@ package com.examplecics.demo.controller;
 import br.com.viavarejo.application.service.JornadaCicsService;
 import br.com.viavarejo.cics.book.BookContexto;
 import com.examplecics.demo.config.ConfigCics;
-import com.examplecics.demo.dto.CancelamentoSTDEntradaDTO;
-import com.examplecics.demo.dto.CancelamentoSTdSaida;
-import com.examplecics.demo.dto.ResponseDTO;
-import com.examplecics.demo.dto.TransferenciaCargaEntradaDto;
+import com.examplecics.demo.dto.*;
 import com.examplecics.demo.exception.FailedException;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
@@ -91,11 +88,77 @@ public class DemoController {
 
     }
 
+    @GetMapping("/teste-transf")
+    public ResponseEntity<?> tranf(){
+
+        var response = new ResponseDTO();
+
+        TransferenciaCargaEntradaDto transferenciaCargaEntradaDto = new TransferenciaCargaEntradaDto();
+        transferenciaCargaEntradaDto.setSolicitacao("D");
+        transferenciaCargaEntradaDto.setOperacao("T");
+        transferenciaCargaEntradaDto.setMotivo(222);
+        transferenciaCargaEntradaDto.setCargaOrigem(44);
+        transferenciaCargaEntradaDto.setCargaDestino(55);
+        transferenciaCargaEntradaDto.setStatusDocumento("std");
+        transferenciaCargaEntradaDto.setDocumento(66);
+        transferenciaCargaEntradaDto.setComplementoDocumento(77);
+        transferenciaCargaEntradaDto.setEmpresaFuncionario(33);
+        transferenciaCargaEntradaDto.setCodigoFuncionario(111);
+
+        TransferenciaCargaSaidaDto saida = null;
+        try {
+            saida = transfService(transferenciaCargaEntradaDto);
+
+            response.setData( saida );
+
+            if(saida.getCodigoRetorno().equals(0)) {
+
+                response.setMessage("Teste com sucesso");
+                response.setStatus("SUCESSO");
+                response.setCode("0");
+
+                String responseJson= new Gson().toJson(response);
+                return new ResponseEntity(response, HttpStatus.OK);
+            }else {
+
+                response.setMessage("Falhou");
+                response.setStatus("Erro");
+                response.setCode("1");
+
+                String responseJson= new Gson().toJson(response);
+                return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } catch (Exception e) {
+            CancelamentoSTdSaida data = CancelamentoSTdSaida.builder()
+                    .mensagem(e.getMessage())
+                    .codigoErroValidacao(" ")
+                    .codigoRetornoSQLCode(1).build();
+            response.setData(data);
+            response.setMessage("Falhou");
+            response.setCode("1");
+
+            String responseJson= new Gson().toJson(response);
+            return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
     public CancelamentoSTdSaida cancelarSTD(CancelamentoSTDEntradaDTO entrada) throws FailedException {
         CancelamentoSTdSaida saida = null;
         try {
             var contexto = new BookContexto();
             saida = (CancelamentoSTdSaida) cicsService.executarChamadaCics(contexto, ConfigCics.CANCELAR_STD, entrada);
+            return saida;
+        }catch (Exception e) {
+            throw new FailedException(e.getMessage(), e);
+        }
+    }
+
+    public TransferenciaCargaSaidaDto transfService(TransferenciaCargaEntradaDto entrada) throws FailedException {
+        TransferenciaCargaSaidaDto saida = null;
+        try {
+            var contexto = new BookContexto();
+            saida = (TransferenciaCargaSaidaDto) cicsService.executarChamadaCics(contexto, ConfigCics.TRANSFERENCIA_CARGA, entrada);
             return saida;
         }catch (Exception e) {
             throw new FailedException(e.getMessage(), e);
